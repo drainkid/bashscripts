b92795b18f267d671fb985beebd2dedc899dacbf
drainkid <91513587+drainkid@users.noreply.github.com>
2022-03-19 00:48:29 +1000

    Add files via upload

---
+++ 2/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "ID пользователя: $UID" 
+
+shpak1=$((5+3)) 
+
+echo " ya vkinul shpakov:"$shpak1 
+
+echo " XdXDDD" >>shpak.txt
+
+vkid="shpak.txt"
+
+find . -type f -name $vkid
+
+echo "че за файл?"
+
+file $vkid
+
+
+
+
